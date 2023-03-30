69fc4fb16d867c6be51e08ded32edf9d0ff8dda1
Remmy <akeemquadri00@gmail.com>
2023-03-30 22:32:12 +0100

    Added the Javascript

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+const submitBtn = document.querySelector('.btn-submit');
+const firstContainer = document.querySelector('.general-box');
+const secondContainer = document.querySelector('.second-container');
+
+submitBtn.addEventListener('click', submitAction);
+
+function submitAction() {
+  secondContainer.classList.toggle('hidden');
+  firstContainer.classList.toggle('hidden');
+}
